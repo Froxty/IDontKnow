31aafe3a740f1d1c7865d059e80f07e12d673b60
Froxty <47831455+Froxty@users.noreply.github.com>
2020-07-17 12:28:54 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var familyDinner = ["Grilled Chicken with Rice and Beans", "Grilled Chicken Salad", "Sausage with Veggie Paste"];
+var dinnerChoice = {
+  recipeName: "Grilled Chicken with Rice and Beans",
+  cookTime: "30mins",
+  proteinType: "Chicken",
+  mealType: "Dinner"
+};
